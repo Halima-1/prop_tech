633fefdb2a0778b40d28f03b6b2676b275eff123
dahunsi halima <dahunsiolajumoke18@gmail.com>
2024-10-12 05:35:42 +0100

    added some files

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
     );
   };
   return (
-      <BrowserRouter>
+      <BrowserRouter basename='prop_tech'>
       <Routes>
         <Route path='/' element={<Layout />}> 
         <Route path='/' element={<Home />} />
