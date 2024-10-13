7e56615aed9aafa1612c2e7ef15d984d8a781230
dahunsi halima <dahunsiolajumoke18@gmail.com>
2024-10-13 13:35:38 +0100

    worked on the landing page media queries

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 // import { useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Outlet } from 'react-router-dom'
-// import './App.css'
-import Header from './component/layout/header';
-import Footer from './component/layout/footer';
-import Home from './component/home/landing'
-import Login from './component/form/login';
-import Register from './component/form/register';
-import Landlord from './component/home/landlord';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import Header from "./component/layout/header";
+import Footer from "./component/layout/footer";
+import Home from "./component/home/landing";
+import Login from "./component/form/login";
+import Register from "./component/form/register";
+import Landlord from "./component/home/landlord";
 function App() {
-  // const [count, setCount] = useState(0)
   function Layout() {
     return (
       <>
@@ -18,18 +16,18 @@ function App() {
         <Footer />
       </>
     );
-  };
+  }
   return (
-      <BrowserRouter basename='prop_tech'>
+    <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Layout />}> 
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/register' element={<Register/>}/> 
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Route>
       </Routes>
-      </BrowserRouter>
-  )
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/component/home/landing.jsx
+++ src/component/home/landing.jsx
@@ -1,104 +1,94 @@
-import "./landing.scss"
-import Tenant from "./tenant"
-import Agent from "./agent"
-import Landlord from "./landlord"
-import { useState } from "react"
-import { replace, useNavigate } from "react-router-dom"
-function Home(){
-    let [displayTenant, setDisplayTenant]= useState(true)
-     let [displayAgent, setDisplayAgent]= useState(false)
-     let [displayLandlord, setDisplayLandlord]= useState(false)
-     const [category, SetCategory] =useState("Tenant")
-     localStorage.setItem("category", category)
-// const category ="Tenant"
-    //  const navigate =useNavigate()
-    const handleTenantClick =()=>{
-        if(displayTenant){
-           let displayAgent= false 
-           let displayLandlord=false
-           setDisplayAgent(displayAgent)
-           setDisplayLandlord(displayLandlord)
-           setDisplayTenant(displayTenant)
-           console.log('display tenant')
-           SetCategory("Tenant")
-        //    const category ="Tenant"
-           localStorage.setItem("category", category)
-
-        }
-
-        else{let displayTenant =true
-            let displayAgent= false 
-            let displayLandlord=false
-            setDisplayAgent(displayAgent)
-            setDisplayLandlord(displayLandlord)
-            setDisplayTenant(displayTenant)
-            SetCategory("Tenant")
-            localStorage.setItem("category", category)
-
-        }
+import "./landing.css";
+import Tenant from "./tenant";
+import Agent from "./agent";
+import Landlord from "./landlord";
+import { useState } from "react";
+import { replace, useNavigate } from "react-router-dom";
+function Home() {
+  let [displayTenant, setDisplayTenant] = useState(true);
+  let [displayAgent, setDisplayAgent] = useState(false);
+  let [displayLandlord, setDisplayLandlord] = useState(false);
+  const [category, SetCategory] = useState("Tenant");
+  localStorage.setItem("category", category);
+  // const category ="Tenant"
+  //  const navigate =useNavigate()
+  const handleTenantClick = () => {
+    if (displayTenant) {
+      let displayAgent = false;
+      let displayLandlord = false;
+      setDisplayAgent(displayAgent);
+      setDisplayLandlord(displayLandlord);
+      setDisplayTenant(displayTenant);
+      console.log("display tenant");
+      SetCategory("Tenant");
+      //    const category ="Tenant"
+      localStorage.setItem("category", category);
+    } else {
+      let displayTenant = true;
+      let displayAgent = false;
+      let displayLandlord = false;
+      setDisplayAgent(displayAgent);
+      setDisplayLandlord(displayLandlord);
+      setDisplayTenant(displayTenant);
+      SetCategory("Tenant");
+      localStorage.setItem("category", category);
     }
+  };
 
-    const handleLandlordClick =()=>{ 
-        if(displayLandlord){
-           let displayAgent= false 
-           let displayTenant=false
-           setDisplayAgent(displayAgent)
-           setDisplayTenant(displayTenant)
-           console.log('display landlord')
-        } 
-        else{displayLandlord =true
-            let displayAgent= false 
-           let displayTenant=false
-           setDisplayAgent(displayAgent)
-           setDisplayTenant(displayTenant)
-            setDisplayLandlord(displayLandlord)
-            SetCategory("Landlord")
-            localStorage.setItem("category", category)
-
-            
-        }
+  const handleLandlordClick = () => {
+    if (displayLandlord) {
+      let displayAgent = false;
+      let displayTenant = false;
+      setDisplayAgent(displayAgent);
+      setDisplayTenant(displayTenant);
+      console.log("display landlord");
+    } else {
+      displayLandlord = true;
+      let displayAgent = false;
+      let displayTenant = false;
+      setDisplayAgent(displayAgent);
+      setDisplayTenant(displayTenant);
+      setDisplayLandlord(displayLandlord);
+      SetCategory("Landlord");
+      localStorage.setItem("category", category);
     }
-         const handleAgentClick =()=>{
-            if(displayAgent){
-               let displayTenant= false 
-               let displayLandlord=false
-               setDisplayAgent(displayAgent)
-               setDisplayLandlord(displayLandlord)
-               setDisplayTenant(displayTenant)
+  };
+  const handleAgentClick = () => {
+    if (displayAgent) {
+      let displayTenant = false;
+      let displayLandlord = false;
+      setDisplayAgent(displayAgent);
+      setDisplayLandlord(displayLandlord);
+      setDisplayTenant(displayTenant);
 
-               console.log('display agent')
-            } 
-            else{let displayAgent=true
-                let displayLandlord= false 
-           let displayTenant=false
-           setDisplayAgent(displayAgent)
-           setDisplayTenant(displayTenant)
-            setDisplayLandlord(displayLandlord)
-            SetCategory("Agent")
-            localStorage.setItem("category", category)
+      console.log("display agent");
+    } else {
+      let displayAgent = true;
+      let displayLandlord = false;
+      let displayTenant = false;
+      setDisplayAgent(displayAgent);
+      setDisplayTenant(displayTenant);
+      setDisplayLandlord(displayLandlord);
+      SetCategory("Agent");
+      localStorage.setItem("category", category);
+    }
+  };
 
-            }
-        }
-       
-    return(
-        
-        <>
-        <section className="hero">
-            <div className="hero_div">
-                <button onClick={handleTenantClick}>Tenant</button>
-                    {/* {display && (<Tenant/>)} */}
-                <button onClick={handleLandlordClick}>Landlord</button>
-                <button onClick={handleAgentClick}>Agent</button>
-
-
-            </div>
-            {        displayTenant? <Tenant /> : null   }
-                    {        displayAgent? <Agent/> : null   }
-                    {        displayLandlord? <Landlord/> : null   }
-        </section>
-        </>
-        
-    )
+  return (
+    <>
+      <section className="hero">
+        <div className="hero_div">
+          <button onClick={handleTenantClick}>Tenant</button>
+          {/* {display && (<Tenant/>)} */}
+          <button onClick={handleLandlordClick}>Landlord</button>
+          <button onClick={handleAgentClick}>Agent</button>
+        </div>
+        {displayTenant ? <Tenant /> : null}
+        {displayAgent ? <Agent /> : null}
+        {displayLandlord ? <Landlord /> : null}
+      </section>
+    </>
+  );
 }
 
-export default Home
+export default Home;
